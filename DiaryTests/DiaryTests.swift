8a5f2871532eb7a5e98c735d19269e8b3b6b5076
Gunhan Gulsoy <gunhangulsoy@gmail.com>
2025-08-09 22:21:31 -0700

    Initial Commit

---
+++ DiaryTests/DiaryTests.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryTests.swift
+//  DiaryTests
+//
+//  Created by Günhan Gülsoy on 8/9/25.
+//
+
+import Testing
+
+struct DiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
